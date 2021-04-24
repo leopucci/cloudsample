df26feb2f9ca423e202e2ba5fb9a34c2ea3bfbd7
Leonardo Pucci <leopucci@gmail.com>
2021-04-24 18:08:54 -0300

    Adicionando api jwt com refresh token
    https://jasonwatmore.com/post/2020/06/17/nodejs-mongodb-api-jwt-authentication-with-refresh-tokens
    https://github.com/cornflourblue/node-mongo-jwt-refresh-tokens-api

---
+++ 2_aplicativo/pocketcloud/android/app/src/main/kotlin/com/example/pocketcloud/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pocketcloud
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- 2_aplicativo/pocketcloud/test/widget_test.dart
+++ 2_aplicativo/pocketcloud/test/widget_test.dart
@@ -7,12 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:modulo4/main.dart';
+
+import 'package:pocketcloud/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Modulo4());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
