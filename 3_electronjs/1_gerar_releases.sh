b59a61e1caabe3d3614b912397e43857d83bb30d
Leonardo Pucci <leopucci@gmail.com>
2021-06-23 11:04:39 -0300

    .

--- 3_electronjs/1_gerar_releases.sh
+++ 3_electronjs/1_gerar_releases.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rm -rf out
 npm run pack
+git fetch
+git pull
 git add 'out/Pocket.Cloud Setup 1.0.0.exe'
 git commit -m 'Commit automatico do Mac gerando release'
 git push 'https://leopucci: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@github.com/leopucci/pocketcloud.git/'
