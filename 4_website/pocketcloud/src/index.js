b842e35856f83f6773ddf76e4d426576ded109e6
Leonardo Pucci <leopucci@gmail.com>
2021-07-14 16:01:07 -0300

    Adicionando o basico

--- 4_website/pocketcloud/src/index.js
+++ 4_website/pocketcloud/src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import thunkMiddleware from 'redux-thunk'
+import persistState from 'redux-localstorage'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import rootReducer from './Redux'
+import App from './Containers/App';
 import reportWebVitals from './reportWebVitals';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(
+      thunkMiddleware
+    ),
+    persistState(/*paths, config*/)
+  )
+)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
