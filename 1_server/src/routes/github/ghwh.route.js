fff6e825e44045b0f0bafbec05e16a8a2c7e71e0
Leonardo Pucci <leopucci@gmail.com>
2021-08-31 11:02:20 -0300

    Update ghwh.route.js

--- 1_server/src/routes/github/ghwh.route.js
+++ 1_server/src/routes/github/ghwh.route.js
@@ -1,41 +1,73 @@
 const express = require('express');
-const Joi = require('joi');
+const httpError = require('http-errors');
 const httpStatus = require('http-status');
-const crypto = require('crypto');
-const validate = require('../../middlewares/validate');
-const webhookMiddleware = require('x-hub-signature').middleware;
-const bodyParser = require('body-parser');
+const { signer } = require('x-hub-signature');
+const { exec } = require('child_process');
+const { enviaNotificacaoSite, enviaNotificacaoApi } = require('../utils/notify');
 
 const router = express.Router();
-
 const SECRET_CONFIGURADO_NO_GITHUB = 'SECRET_CONFIGURADO_NO_GITHUB';
-const githubWebhook = async (req, res) => {
-  // console.log(req.body);
-  console.log(req.body);
+const GITHUB_REPOSITORIES_TO_DIR = {
+  'rwieruch/my-website-one-on-github': '/opt/reactbuildtemp',
+  'rwieruch/my-website-two-on-github': '/home/rwieruch/my-website-two',
+};
 
-  const isMaster = req.body?.ref === 'refs/heads/master';
-  console.log(isMaster);
-
-  const directory = GITHUB_REPOSITORIES_TO_DIR[body?.repository?.full_name];
-
-  if (isAllowed && isMaster && directory) {
-    try {
-      exec(`cd ${directory} && bash deploy.sh`);
-    } catch (error) {
-      console.log(error);
-    }
+const verifySignature = function (req, res, next) {
+  const sign = signer({ algorithm: 'sha1', secret: SECRET_CONFIGURADO_NO_GITHUB });
+  const { rawBody } = req;
+  if (!rawBody) {
+    enviaNotificacaoApi(
+      'Falha na configuração do  github webhook x-hub-signature, alguem tirou o app.use(express.json({ verify: webhookMiddleware.extractRawBody }));'
+    );
+    return next(httpError(500, 'Missing req.rawBody, see the x-hub-signature readme'));
   }
 
-  res.status(httpStatus.OK).send();
+  const signature = req.header('X-Hub-Signature');
+
+  if (!signature) {
+    enviaNotificacaoApi('Falha na configuração do github webhook Missing X-Hub-Signature header');
+    return next(httpError(400, 'Missing X-Hub-Signature header'));
+  }
+  if (signature) {
+    const body = Buffer.from(rawBody);
+
+    if (signature !== sign(body)) {
+      enviaNotificacaoApi(
+        'Falha no github webhook A assinatura nao bateu, alguem tentando algo indevido? Ou voce mudou o secret no site do github?'
+      );
+      return next(httpError(400, 'Invalid X-Hub-Signature'));
+    }
+  }
+};
+const githubWebhook = async (req, res) => {
+  if (await verifySignature(req)) {
+    console.log(req.body);
+
+    const isMaster = req.body?.ref === 'refs/heads/master';
+    console.log(isMaster);
+
+    const directory = GITHUB_REPOSITORIES_TO_DIR[req.body?.repository?.full_name];
+
+    if (isMaster && directory) {
+      try {
+        exec(`cd ${directory} && bash deploy.sh`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    res.status(httpStatus.OK).send();
+  } else {
+  }
 };
 
 router.post(
   '/githubwebhook',
-  webhookMiddleware({
+  /* webhookMiddleware({
     algorithm: 'sha1',
     secret: SECRET_CONFIGURADO_NO_GITHUB,
     require: true,
-  }),
+  }), */
   githubWebhook
 );
 
