1f7432ea26acbf7f07e0401829bb6a8ff56cc9f4
Leonardo Pucci <leopucci@gmail.com>
2021-09-03 11:11:46 -0300

    Update error.js

--- 1_server/src/middlewares/error.js
+++ 1_server/src/middlewares/error.js
@@ -4,13 +4,14 @@ const safeJsonStringify = require('safe-json-stringify');
 const config = require('../config/config');
 const logger = require('../config/logger');
 const ApiError = require('../utils/ApiError');
+const ApiNotFoundError = require('../utils/errors/ApiNotFoundError');
 const { enviaNotificacaoSite, enviaNotificacaoApi, canais } = require('../utils/notify');
 
 const errorConverter = (err, req, res, next) => {
-  const errString = JSON.stringify(err);
+  const errString = safeJsonStringify(err);
   // enviaNotificacaoApi(`Caiu no errorConverter \n${errString}`, canais.PocketErrosHttp);
   let error = err;
-  if (!(error instanceof ApiError)) {
+  if (!(error instanceof ApiError) || !(error instanceof ApiNotFoundError)) {
     const statusCode =
       error.statusCode || error instanceof mongoose.Error ? httpStatus.BAD_REQUEST : httpStatus.INTERNAL_SERVER_ERROR;
     const message = error.message || httpStatus[statusCode];
@@ -21,7 +22,7 @@ const errorConverter = (err, req, res, next) => {
 
 // eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
-  const errString = JSON.stringify(err);
+  const errString = safeJsonStringify(err);
   // enviaNotificacaoApi(`Caiu no errorHandler \n${errString}`, canais.PocketErrosHttp);
   let { statusCode, message } = err;
   if (config.env === 'production' && !err.isOperational) {
