ffb89dd83febf4d6b60a81e719c01f9052f9acdf
Leonardo Pucci <leopucci@gmail.com>
2021-09-16 10:07:05 -0300

    Update RecaptchaTermsOfService.js

--- 4_website/pocketcloud/src/Components/RecaptchaTermsOfService.js
+++ 4_website/pocketcloud/src/Components/RecaptchaTermsOfService.js
@@ -12,6 +12,13 @@ const useStyles = makeStyles({
 });
 
 export default function RecaptchaTermsOfService() {
+  // ESTE EH O ORIGINAL QUE EU ACHEI NA NET
+  /* This site is protected by reCAPTCHA{"\n"} and the Google&nbsp;
+      <a href="https://policies.google.com/privacy">Privacy Policy</a> and&nbsp;
+      <a href="https://policies.google.com/terms">Terms of Service</a> apply. */
+
+  // ESTE EH O QUE EU ENCONTREI EM PROD NO STRIPO.EMAIL
+  // This page is protected by reCAPTCHA, and subject to the Google Privacy Policy and Terms of Service.
   // eslint-disable-next-line no-unused-vars
   const classes = useStyles();
   return (
@@ -21,9 +28,12 @@ export default function RecaptchaTermsOfService() {
       align="center"
       style={{ whiteSpace: "pre-line", fontSize: 10 }}
     >
-      This site is protected by reCAPTCHA{"\n"} and the Google&nbsp;
-      <a href="https://policies.google.com/privacy">Privacy Policy</a> and&nbsp;
-      <a href="https://policies.google.com/terms">Terms of Service</a> apply.
+      This site is protected by reCAPTCHA{"\n"} and subject to the&nbsp;
+      <a href="https://policies.google.com/privacy">
+        Google Privacy Policy
+      </a>{" "}
+      and&nbsp;
+      <a href="https://policies.google.com/terms">Terms of Service</a>.
     </Typography>
   );
 }
